38ef63884a97d7bcb74a6b9d96c72cdd47654940
tarrellriley <tarrellriley@gmail.com>
2018-08-30 20:40:45 -0500

    still working

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -2,15 +2,36 @@
 int main()
 {
 	char input;
-	int gameNumber;
+	int gameNumber = 50;
 
-	std::cout << " what to play a game think of a number 1 - 100 " << std::endl;
-	std::cout << " im going to guess 50 ";
-	std::cout << " is that < \ > or = ";
-	std::cin >> input;
-	if (input == '<' ) {
-		input + 10;
-	}
-
+	std::cout << " what to play a game think of a number between 1 and 100 " << std::endl;
 	
+	system("pasue");
+	
+	std::cout << " im going to guess 50 ";
+	std::cout << " is that < , > or = ";
+	std::cin >> input;
+	// if you have to higer
+	if (input == '<' ) 
+	{
+		std::cout << gameNumber + 10;
+		std::cout << "is thse your number";
+		std::cout << " < if its less then" 
+			<<std::cout << " > if its greater then"
+			<< std::cout << "and = if i got it right" << std::endl;
+		{
+			std::cin >> input;
+			if ( input ==)
+		}
+	}
+	else  if ( input == '>')
+	{
+		 std::cout << gameNumber - 10;
+	}
+	else
+	{
+		std::cout << "yes i got it right good game lets play agein " << std::endl;
+	}
+	
+	system("pasue");
 }
